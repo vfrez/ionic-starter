bf55171b043df7b793400bff5355bec5180befda
João Antonio Maruti Milagres <joaoantoniomaruti@gmail.com>
2016-08-09 20:24:51 +0000

    Adicionando comentarios para ser útil

--- www/views/home/home.js
+++ www/views/home/home.js
@@ -4,15 +4,20 @@
 angular.module('app.homeCtrl', [])
 
 .controller('home.ctrl', function($scope, $state, $ionicSideMenuDelegate, $timeout){
+    /**
+     * @todo - Responsalvel para escutar se o menu está aberto ou não e jogar um valor booleano para a váriavel de controle na view
+     */
     $scope.$watch(function (){
         return $ionicSideMenuDelegate.isOpenLeft();
     }, function (isOpen) {
         $timeout(function(){
             $scope.menu = isOpen;
         }, 160);
-  });
-  
-  $scope.openMenu = function(){
-    $ionicSideMenuDelegate.toggleLeft();
-  };
+    });
+    /**
+     * @todo - Abre o sideMenu
+     */
+    $scope.openMenu = function(){
+        $ionicSideMenuDelegate.toggleLeft();
+    };
 })
